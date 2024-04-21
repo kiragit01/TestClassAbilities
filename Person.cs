ce4e7330ab4fb38d2260395ea6d853389ff9f05a
kiragit01 <kiragithub@gmail.com>
2024-04-21 14:34:34 +0300

    Добавьте файлы проекта.

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maga_test
+{
+    public class Person
+    {
+        public string Name { get; private set; }
+        public string FirstName { get; private set; }
+        public DateTime Birthday { get; private set; }
+        public int Age { get; private set; }
+
+        public Person(string name, string firstName, DateTime birthday)
+        {
+            Name = name;
+            FirstName = firstName;
+            Birthday = birthday;
+            Age = (int)Math.Floor((DateTime.Now - birthday).TotalDays / 365.2425);
+        }
+        public string Print()
+        {
+            return $"└--- {FirstName} {Name} {Birthday.ToShortDateString()} {Age}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maga_test
+{
+    internal class Program
+    {
+        public static List<Person> persons = new List<Person>();
+        static void Main()
+        {
+            WriteActionList();
+            char action = char.Parse(Console.ReadLine().ToLower());
+            while (action != 'в')
+            {
+                switch (action)
+                {
+                    case 'а':
+                        AddPerson();
+                        break;
+                    case 'п':
+                        AllPersons();
+                        break;
+                    case 'х':
+                        WriteActionList();
+                        break;
+                }
+                Console.WriteLine();
+                action = char.Parse(Console.ReadLine().ToLower());
+            }
+            
+            Console.ReadKey();
+        }
+
+        static void WriteActionList()
+        {
+            Console.WriteLine("А - добавить пользователя");
+            Console.WriteLine("П - показать всех пользователей");
+            Console.WriteLine("Х - лист действий");
+            Console.WriteLine("В - выход");
+        }
+
+        static void AllPersons()
+        {
+            Console.WriteLine("Список пользователей: ");
+            Console.WriteLine("Фамилия || Имя || Дата рождения || Возраст");
+            foreach (Person person in persons)
+            { 
+                Console.WriteLine(person.Print());
+            }
+        }
+
+
+        static void AddPerson()
+        {
+            try
+            {
+                Console.Write("Имя: ");
+                string name = Console.ReadLine();
+                Console.Write("Фамилия: ");
+                string firstname = Console.ReadLine();
+                Console.Write("Дата рождения (день.месяц.год): ");
+                DateTime dateTime = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Пользователь успешно добавлен!");
+                Person person = new Person(name, firstname, dateTime);
+                persons.Add(person);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+
+}
